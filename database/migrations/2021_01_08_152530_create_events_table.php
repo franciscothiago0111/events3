7620a5bab4b89755d08f49ffefc0b4672c39c6cb
thiagosennin1 <thiagosennin1@gmail.com>
2021-01-14 18:56:49 -0300

    first commit

---
+++ database/migrations/2021_01_08_152530_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string("title");
+            $table->text("description");
+            $table->string("city");
+            $table->boolean("private");
+            $table->dateTime('date');
+            $table->json('items');
+            $table->string('image');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
